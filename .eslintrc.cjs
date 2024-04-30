85c7941d3e2d618fa04554656e494caf97b80ec8
Elvis Saavedra <elvis.saavedra@nearsure.com>
2024-04-30 15:26:58 -0500

    added eslint rules and prettier formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,20 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'comma-dangle': 'off',
   }
 }
